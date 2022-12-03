8b1ce18eb0afed2fba8334e42cf26468d41e42fb
Raffid Ahmad <raffidahmad@gmail.com>
2022-12-03 23:52:39 +0500

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/test', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
