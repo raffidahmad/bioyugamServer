e34d0dc37982a8b10e7b306352841b9c7dae9c27
Raffid Ahmad <raffidahmad@gmail.com>
2022-12-04 00:23:52 +0500

    test juju

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var cors = require('cors');
+var parseString = require('xml2js').parseString;
 const app = express();
 const port = 3000;
 var allowedOrigins = ['http://localhost:3000',
@@ -13,9 +14,35 @@ var allowedOrigins = ['http://localhost:3000',
             } return callback(null, true);
         }
     }));
+
+
 app.get('/', (req, res) => {
     //send as json
     res.json({ message: 'Hello World!' });
+});
+
+app.get('/jobs', (req, res) => {
+    fetch(`http://api.juju.com/jobs?partnerid=a22e764782e9cf803d160f9417cce8fd&
+    k&c=biotech-pharmaceutical+health-care+hospitality+human-resources+nursing&
+    l=Chicago%2C+IL&
+    useragent=Mozilla%2F5.0+(Macintosh%3B+U%3B+Intel+Mac+OS+X+10.6%3B+en-US%3B+rv%3A1.9.1.7)+Gecko%2F20091221+Firefox%2F3.5.7&
+    ipaddress=98.193.69.227` , {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'text/xml'
+        }
+    }).then(response => {
+        return response.text();
+    }).then(responseText => {
+        var xmlData = responseText;
+        parseString(xmlData, function (err, result) {
+            const keys = Object.keys(result);
+            const subKeys = Object.keys(result[keys[0]]);
+            const channel = result[keys[0]][subKeys[1]];
+            const items = channel[0].item;
+            res.json(items);
+        });
+    })
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
