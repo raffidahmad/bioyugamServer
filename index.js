e71efb9154f08df8b558169d3c46335d388bdbc8
Raffid Ahmad <raffidahmad@gmail.com>
2022-12-04 00:00:14 +0500

    cors

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+var cors = require('cors');
 const app = express();
 const port = 3000;
-
+var allowedOrigins = ['http://localhost:3000',
+    'https://bioyugam.vercel.app/']; app.use(cors({
+        origin: function (origin, callback) {    // allow requests with no origin 
+            // (like mobile apps or curl requests)
+            if (!origin) return callback(null, true); if (allowedOrigins.indexOf(origin) === -1) {
+                var msg = 'The CORS policy for this site does not ' +
+                    'allow access from the specified Origin.';
+                return callback(new Error(msg), false);
+            } return callback(null, true);
+        }
+    }));
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
